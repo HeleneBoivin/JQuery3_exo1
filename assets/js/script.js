a5aa3d22031c57c3bed14731efae501887ff142a
HBoivin <boivinhelene@yahoo.com>
2018-11-04 23:09:44 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// // Version 1
+// var num = 1;
+// function counter(num) {
+//   }
+// $(document).ready(function() {
+//   $('button').click(function() {
+//         num++;
+//         /*
+//       Leave
+//     */
+//         $('.count-leave').addClass('count-leave-active');
+//     setTimeout(function() {
+//       $('.count-leave').remove();
+//     }, 250);
+//         /*
+//       Enter
+//     */
+//         $('.count-enter').addClass('count-enter-active');
+//     setTimeout(function() {
+//       $('.count-enter').addClass('count-leave').removeClass('count-enter count-enter-active');
+//       $('body').append('<h1 class="count count-enter">'+ num +'</h1>')
+//     }, 250);
+//       })
+// });
+
+
+$(function(){
+  $("#bouton").click(function(){
+    var result = $("#nombreClick").val();
+    result++;
+    $("#nombreClick").val(result);
+  })
+})
